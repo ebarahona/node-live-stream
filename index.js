9a6e3013c8b9b98e0fd3acacba36c2dde74d8f64
SamHeutmaker <samheutmaker@gmail.com>
2016-01-13 14:14:31 -0800

    First working version

--- index.js
+++ index.js
@@ -5,58 +5,57 @@ const http2 = require('http');
 const io = require('socket.io').listen(http);
 const fs = require('fs');
 const events = require('events')
-const EE = new events.EventEmitter();
 
 var port = process.env.PORT || 8888;
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
-	res.redirect('index.html');
-});
-
-http.listen(port,function(){
-	console.log('started');
-});
-
-io.on('connection', function(socket){
-	socket.on('stream', function(data){
-		socket.broadcast.emit('stream', data);
-	})
+// 
+app.get('/', function(req, res) {
+	res.redirect('link.html');
 })
+// 
+app.get('/video', function(req, res) {
+  res.redirect('video.html');
+});
+
+// 
+app.post('/videoin', function(req, res) {
+	  req.on('data', function(data) {
+  		var frame = new Buffer(data).toString('base64');
+    	io.sockets.emit('canvas', frame);
+  });
+});
+
+http.listen(port, function() {
+  console.log('started');
+});
+
+io.on('connection', function(socket) {
+  socket.on('stream', function(data) {
+    socket.broadcast.emit('stream', data);
+  });
+});
 
 
- var ffmpegServer = http2.createServer(function(req, res) {
-   req.on('data', function(data) {
-
-     var frame = new Buffer(data).toString('base64');
-
-     io.sockets.emit('canvas', frame);
-
-   })
- }).listen(3000);
-
-
-// EE.on('incomingData', function(frameData) {
-//   console.log(frameDataexk);
-// });
 
 
 
-
-// var PORT = 33333;
+// var PORT = 3000;
 // var HOST = '127.0.0.1';
 
 // var dgram = require('dgram');
 // var server = dgram.createSocket('udp4');
 
-// server.on('listening', function () {
-//     var address = server.address();
-//     console.log('UDP Server listening on ' + address.address + ":" + address.port);
+// server.on('listening', function() {
+//   var address = server.address();
+//   console.log('UDP Server listening on ' + address.address + ":" + address.port);
 // });
 
-// server.on('message', function (message, remote) {
-//     console.log(message);
+// server.on('message', function(data, remote) {
+
+//   var frame = new Buffer(data).toString('base64');
+//   io.sockets.emit('canvas', frame);
 
 // });
 
--- public/connect.js
+++ public/connect.js
@@ -3,7 +3,7 @@ var connect = null;
 
 $(document).bind('ready', function () {
 	console.log("streamClient ready!");	
-	connect = new Connect('127.0.0.1',8888);
+	connect = new Connect('',"");
 	connect.initSocket();
 });
 
@@ -18,27 +18,39 @@ function Connect(serverIP, serverPort) {
 // open web socket
 Connect.prototype.initSocket = function() {
 
-    this.socket = io.connect('http://'+this.serverIP+":"+this.serverPort);
+    this.socket = io.connect(window.location.orgin);
 
 	// on connection to server
 	this.socket.on('connect', function() {
-	    console.log("socket connected to http://"+connect.serverIP+":"+connect.serverPort);
+	    console.log("socket connected to http://"+window.location.orgin);
 	});
 
 	//read video stream
 	this.socket.on('canvas', function(data) {
 		//console.log(data);
 		try {
-			// 1. method: draw on canvas
-			/*var canvas = document.getElementById('videostream');
-			var context = canvas.getContext('2d');
-			var imageObj = new Image();
-			imageObj.src = "data:image/jpeg;base64,"+data;
-			imageObj.onload = function(){
-				context.height = imageObj.height;
-				context.width = imageObj.width;
-				context.drawImage(imageObj,0,0,context.width,context.height);
-			}*/
+
+			// draw(incoming);
+			// console.log('incoming');
+
+			// function draw(data) {
+
+			// 	setTimeout(function(){
+			// 		console.log('should draw');
+			// 		// 1. method: draw on canvas
+			// 		var canvas = document.getElementById('videostream');
+			// 		var context = canvas.getContext('2d');
+			// 		var imageObj = new Image();
+			// 		imageObj.src = "data:image/jpeg;base64,"+data;
+			// 		imageObj.onload = function(){
+			// 			context.height = imageObj.height;
+			// 			context.width = imageObj.width;
+			// 			context.drawImage(imageObj,0,0,context.width,context.height);
+			// 		}
+
+			// 	},33);
+			// };
+	
 			// 2. method: draw as CSS background
 			$('#videostream').css('background', 'transparent url(data:image/jpeg;base64,'+data+') top left / 100% 100% no-repeat');
 		} catch(e){
